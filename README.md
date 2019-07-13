184fb3f3f1be30836c636a89c0cd6d1827680eca
Mohammed <m.aboabdo@yahoo.com>
2019-07-13 18:07:30 +0200

    url-shortener serveice v1.0.0

---
+++ README.md
@@ -0,0 +1,22 @@
+##URL Shortener Service
+
+API to create short urls using Node, Express and MongoDB
+
+
+#npm install 
+
+# Edit the default.json file with your mongoURI and baseUrl
+# Use production.json in production env
+
+
+#How to Run
+
+npm start Or npm run dev
+
+
+
+#To create short url 
+
+POST api/url/shorten
+
+{"longUrl":"Url you need"}
---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const connectDB = require('./config/db');
+const app = express();
+//Connect to database
+connectDB();
+
+app.use(express.json({ extented: false })); //allow us to accept json data on our Api
+app.use('/', require('./routes/index'));
+app.use('/api/url', require('./routes/url'));
+const PORT = 5000;
+
+app.listen(PORT, () => console.log(`Server Running on port ${PORT}`));
